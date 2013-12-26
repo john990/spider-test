5c2d37529992f33a0dfa70b6a7832e34eb119e0c
kai.wang <kai.wang.john@gmail.com>
2013-12-26 16:48:30 +0800

    添加基础类

--- src/us/codecraft/webmagic/samples/OschinaBlogPageProcesser.java
+++ src/us/codecraft/webmagic/samples/OschinaBlogPageProcesser.java
@@ -3,8 +3,10 @@ package us.codecraft.webmagic.samples;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.model.AfterExtractor;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,11 +18,15 @@ public class OschinaBlogPageProcesser implements PageProcessor {
 
     @Override
     public void process(Page page) {
-        List<String> links = page.getHtml().links().regex("http://my\\.oschina\\.net/flashsword/blog/\\d+").all();
+//        List<String> links = page.getHtml().links().regex("http://my\\.oschina\\.net/flashsword/blog/\\d+").all();
+	    List<String> links = new ArrayList<String>();
+	    links.add("http://my.oschina.net/flashsword/blog/145796");
         page.addTargetRequests(links);
         page.putField("title", page.getHtml().xpath("//div[@class='BlogEntity']/div[@class='BlogTitle']/h1/text()").toString());
         page.putField("content", page.getHtml().xpath("//div[@class='BlogContent']/tidyText()").toString());
         page.putField("tags",page.getHtml().xpath("//div[@class='BlogTags']/a/text()").all());
+	    // 抓取图片
+	    page.putField("images",page.getHtml().xpath("//div[@class='BlogContent']//img[not(contains(@src,'img_proxy'))]/@src").all());
     }
 
     @Override
