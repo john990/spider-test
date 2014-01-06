e2ebd6ab867d686dd1924e444e7ec5e496927e94
kai.wang <kai.wang.john@gmail.com>
2014-01-06 16:49:44 +0800

    持久化

---
+++ src/com/spider/bean/Post.java
@@ -0,0 +1,13 @@
+package com.spider.bean;
+
+/**
+ * Created by kai.wang on 1/6/14.
+ */
+public class Post {
+	private String title = "";
+	private String createAt = "";
+	private String tag = "";
+	private String images = "";
+	private String cover = "";
+	private String author = "";
+}
