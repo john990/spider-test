50395444129225f126a86a6d1748492d96696f1c
kai.wang <kai.wang.john@gmail.com>
2014-01-07 13:47:29 +0800

    抓取用户

--- src/com/spider/capture/UserV2EX.java
+++ src/com/spider/capture/UserV2EX.java
@@ -15,17 +15,20 @@ import us.codecraft.webmagic.processor.PageProcessor;
  * Created by kai.wang on 1/7/14.
  */
 public class UserV2EX implements PageProcessor {
-	private Site site = Site.me().setDomain("v2ex.com").addStartUrl("http://v2ex.com/t/95000");
+	private Site site = Site.me().setDomain("v2ex.com").addStartUrl("http://v2ex.com/t/95799");
 	private static List<String> links = new ArrayList<String>();
 	@Override
 	public void process(Page page) {
 		page.addTargetRequests(links);
+
 		User user = new User();
 		String name = page.getHtml().xpath("//div[@class='header']/small[@class='gray']/a/text()").toString();
 		if(name == null) return;
 		user.setName(name);
+
 		String avatar = page.getHtml().xpath("//div[@class='header']//img[@class='avatar']/@src").toString();
 		if(avatar != null) user.setAvatar(avatar.substring(0,avatar.lastIndexOf("?")));
+
 		user.setPassword("666777dfd");
 		user.setEmail("robot@gmail.com");
 		UserDao.saveUniqueUser(user);
@@ -38,7 +41,7 @@ public class UserV2EX implements PageProcessor {
 	}
 
 	public static void main(String[] args) {
-		for(int i=95000;i<96000;i++){
+		for(int i=95800;i<96000;i++){
 			links.add("http://v2ex.com/t/"+i);
 		}
 		Spider.create(new UserV2EX()).run();
