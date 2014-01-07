e949e95120ac9bfa2a29f05e131cf78efa62be21
kai.wang <kai.wang.john@gmail.com>
2014-01-07 10:20:46 +0800

    修改抓取规则

--- src/com/spider/capture/UserV2EX.java
+++ src/com/spider/capture/UserV2EX.java
@@ -15,15 +15,17 @@ import us.codecraft.webmagic.processor.PageProcessor;
  * Created by kai.wang on 1/7/14.
  */
 public class UserV2EX implements PageProcessor {
-	private Site site = Site.me().setDomain("v2ex.com").addStartUrl("http://v2ex.com/t/9000");
+	private Site site = Site.me().setDomain("v2ex.com").addStartUrl("http://v2ex.com/t/95000");
 	private static List<String> links = new ArrayList<String>();
 	@Override
 	public void process(Page page) {
 		page.addTargetRequests(links);
 		User user = new User();
-		user.setName(page.getHtml().xpath("//div[@class='header']/small[@class='gray']/a/text()").toString());
+		String name = page.getHtml().xpath("//div[@class='header']/small[@class='gray']/a/text()").toString();
+		if(name == null) return;
+		user.setName(name);
 		String avatar = page.getHtml().xpath("//div[@class='header']//img[@class='avatar']/@src").toString();
-		user.setAvatar(avatar.substring(0,avatar.lastIndexOf("?")));
+		if(avatar != null) user.setAvatar(avatar.substring(0,avatar.lastIndexOf("?")));
 		user.setPassword("666777dfd");
 		user.setEmail("robot@gmail.com");
 		UserDao.saveUniqueUser(user);
@@ -37,7 +39,7 @@ public class UserV2EX implements PageProcessor {
 	}
 
 	public static void main(String[] args) {
-		for(int i=9000;i<10000;i++){
+		for(int i=95000;i<96000;i++){
 			links.add("http://v2ex.com/t/"+i);
 		}
 		Spider.create(new UserV2EX()).run();
