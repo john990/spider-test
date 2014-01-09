b2f4839bdf440fa3ada008d5feaeff9514089abc
kai.wang <kai.wang.john@gmail.com>
2014-01-09 13:31:44 +0800

    content html

--- src/com/spider/capture/Xinli001Post.java
+++ src/com/spider/capture/Xinli001Post.java
@@ -9,20 +9,21 @@ import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Selectable;
 
 /**
  * Created by kai.wang on 1/9/14.
  * xinli001.com
  */
 public class Xinli001Post implements PageProcessor {
-	private Site site = Site.me().setDomain("xinli001.com").addStartUrl("http://www.xinli001.com/info/");
+	private Site site = Site.me().setDomain("xinli001.com").addStartUrl("http://www.xinli001.com/info/1");
 
 	private static List<String> links = new ArrayList<String>();
 	@Override
 	public void process(Page page) {
 		page.addTargetRequests(links);
 		String title = page.getHtml().xpath("//div[@class='tinfo fl']/h2/text()").toString();
-		String content = page.getHtml().xpath("//div[@class='show_contents']").toString();
+		String content = page.getHtml().xpath("//div[@class='show_contents']/html()").toString();
 		// 3-23
 		String createAt = page.getHtml().xpath("//div[@class='tinfo fl']/span").toString();
 		String tag = page.getHtml().xpath("//div[@class='tags']/a/text()").toString();
@@ -32,7 +33,7 @@ public class Xinli001Post implements PageProcessor {
 
 		Post post = new Post();
 		post.setTitle(title);
-//		post.setContent(content);
+		post.setContent(content);
 		post.setCreateAt(createAt);
 		post.setTag(tag);
 		post.setImages(images);
