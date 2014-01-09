b61dafc6e5fbffe19c077f2de2734ee73f5cc04d
kai.wang <kai.wang.john@gmail.com>
2014-01-09 10:41:37 +0800

    xinli001抓取

--- src/com/spider/bean/Post.java
+++ src/com/spider/bean/Post.java
@@ -5,9 +5,99 @@ package com.spider.bean;
  */
 public class Post {
 	private String title = "";
+	private String content = "";
 	private String createAt = "";
 	private String tag = "";
 	private String images = "";
 	private String cover = "";
 	private String author = "";
+	private String fromName = "";
+	private String fromLink = "";
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(String createAt) {
+		this.createAt = createAt;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getImages() {
+		return images;
+	}
+
+	public void setImages(String images) {
+		this.images = images;
+	}
+
+	public String getCover() {
+		return cover;
+	}
+
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getFromName() {
+		return fromName;
+	}
+
+	public void setFromName(String fromName) {
+		this.fromName = fromName;
+	}
+
+	public String getFromLink() {
+		return fromLink;
+	}
+
+	public void setFromLink(String fromLink) {
+		this.fromLink = fromLink;
+	}
+
+	@Override
+	public String toString() {
+		return "Post{" +
+				"title='" + title + '\'' +
+				", content='" + content + '\'' +
+				", createAt='" + createAt + '\'' +
+				", tag='" + tag + '\'' +
+				", images='" + images + '\'' +
+				", cover='" + cover + '\'' +
+				", author='" + author + '\'' +
+				", fromName='" + fromName + '\'' +
+				", fromLink='" + fromLink + '\'' +
+				'}';
+	}
 }
---
+++ src/com/spider/capture/Xinli001Post.java
@@ -0,0 +1,55 @@
+package com.spider.capture;
+
+import com.spider.bean.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * Created by kai.wang on 1/9/14.
+ * xinli001.com
+ */
+public class Xinli001Post implements PageProcessor {
+	private Site site = Site.me().setDomain("xinli001.com").addStartUrl("http://www.xinli001.com/info/");
+
+	private static List<String> links = new ArrayList<String>();
+	@Override
+	public void process(Page page) {
+		page.addTargetRequests(links);
+		String title = page.getHtml().xpath("//div[@class='tinfo fl']/h2/text()").toString();
+		String content = page.getHtml().xpath("//div[@class='show_contents']").toString();
+		// 3-23
+		String createAt = page.getHtml().xpath("//div[@class='tinfo fl']/span").toString();
+		String tag = page.getHtml().xpath("//div[@class='tags']/a/text()").toString();
+		String images = page.getHtml().xpath("//div[@class='show_contents']//img/@src").toString();
+		String fromName = page.getHtml().xpath("//p[@class='xh-heightlight']/a").toString();
+		String fromLink = page.getHtml().xpath("//p[@class='xh-heightlight']/a/@href").toString();
+
+		Post post = new Post();
+		post.setTitle(title);
+//		post.setContent(content);
+		post.setCreateAt(createAt);
+		post.setTag(tag);
+		post.setImages(images);
+		post.setFromName(fromName);
+		post.setFromLink(fromLink);
+		System.out.println(post.toString());
+	}
+
+	@Override
+	public Site getSite() {
+		return site;
+	}
+
+	public static void main(String[] args){
+		for(int i=1;i<11000;i++){
+			links.add("http://www.xinli001.com/info/"+i);
+		}
+		Spider.create(new Xinli001Post()).run();
+	}
+}
